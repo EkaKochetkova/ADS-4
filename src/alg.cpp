5103a2dcbf8379295ce3c78ae007af866be13767
Кочеткова Екатерина <94628140+EkaKochetkova@users.noreply.github.com>
2022-03-27 21:59:11 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,7 +2,7 @@
 int countPairs1(int *arr, int len, int value) {
    int count = 0;
     for (int i = 0; i < len - 1; ++i) {
-        for (int j = len - 1; j > i; --j) {
+        for (int j = i+1; j < len; ++j) {
             if (arr[i] + arr[j] == value) {
                 ++count;
             }
@@ -36,8 +36,8 @@ int countPairs2(int *arr, int len, int value) {
 }
 int countPairs3(int *arr, int len, int value) {
   int val = 0;
-  int count = 0;
-    for (int k=0; k < len/2; ++k) {
+   int count = 0;
+   for (int k=0; k < len/2+1; ++k) {
        int low = k;
        int top = len - 1;
        val = value - arr[k];
