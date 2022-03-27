45428ccc20c0fa0b9c404bcdf23165af93b65b4f
Кочеткова Екатерина <94628140+EkaKochetkova@users.noreply.github.com>
2022-03-27 23:38:46 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -27,39 +27,33 @@ int countPairs2(int *arr, int len, int value) {
     return 0;
 }
 int countPairs3(int *arr, int len, int value) {
-   int val = 0;
    int count = 0;
-   for (int i=0; i < len/2+1; ++i) {
-       int low = i;
-       int top = len;
-       val = value - arr[i];
-       while (low <= top) {
-           int mid = (top + low) / 2;
-           if (arr[mid] == val) {
-               int i = mid + 1;
-               int j = mid - 1;
-               ++count;
-                while (arr[i] == val || arr[j] == val) {
-                   if (arr[i] == val) {
-                       count += 1;
-                       i += 1;
-                   }
-                   if (arr[j] == val) {
-                       count += 1;
-                       j -= 1;
-                   }                 
-               }
+    for (int i = 0; i < len - 1; i++) {
+        int low = i;
+        int top = len;
+        while (low < top-1) {
+            int mid = (low + top) / 2;
+            if (arr[i] + arr[mid] == value) {
+                ++count;
+                int j = mid + 1;
+                while (arr[i] + arr[j] == value && j < top) {
+                    ++count;
+                    ++j;
+                }
+                j = mid - 1;
+                while (arr[i] + arr[j] == value && j > low) {
+                    ++count;
+                    --j;
+                }
                 break;
-           }
-           if (arr[mid] < val) {
-               low = mid + 1;
-           }
-           if (arr[mid] > val) {
-               top = mid - 1;
-           }
-       }
-   }
-   if (count != 0)
-       return count;
-   return 0; 
+            }
+            if (arr[i] + arr[mid] < value)
+                low = mid;
+            if (arr[i] + arr[mid] > value)
+                top = mid;
+        }
+    }
+    if (count != 0)
+        return count;
+    return 0;
 }
